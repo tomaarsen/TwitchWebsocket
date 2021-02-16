58ef693a39c93d907f739ae94758e24f4175c3bc
CubieDev <cubiegamedev@gmail.com>
2021-02-16 15:24:09 +0100

    Rewrote TwitchWebsocket class for V1.1.0
    Improved logging and reconnecting on timeout/errors

--- TwitchWebsocket/TwitchWebsocket.py
+++ TwitchWebsocket/TwitchWebsocket.py
@@ -1,158 +1,330 @@
 
 import socket, types, threading, time, logging
+from typing import Callable, List, Optional, Union
 
 from TwitchWebsocket.Message import Message
 
-logger = logging.getLogger("TwitchWebsocket")
+logging.basicConfig(level=logging.INFO, format=f'[%(asctime)s] [%(name)-12s] [%(levelname)-8s] - %(message)s')
+logger = logging.getLogger(__name__)
 
 class TwitchWebsocket(threading.Thread):
-    def __init__(self, host, port, chan, nick, auth, callback, capability = None, live = False):
-        assert type(host) == str and type(port) == int and (type(callback) == types.FunctionType or type(callback) == types.MethodType)
+    """
+    TwitchWebsocket class used for connecting a Twitch account to a Twitch channel's chat, 
+    where it can read messages of many kinds, and post messages of its own.
+    """
+    def __init__(self, 
+                 host: str, 
+                 port: str, 
+                 chan: str, 
+                 nick: str, 
+                 auth: str, 
+                 callback: Callable[[Message], None], 
+                 capability: Optional[Union[List[str], str]] = None, 
+                 live: bool = True):
+        """
+        `host`: IRC Host, generally "irc.chat.twitch.tv".
+        `port`: Socket port, generally `6667`.
+        `chan`: Twitch channel of the chat to join, e.g. "#Tom" for www.twitch.tv/tom.
+        `nick`: Twitch account name to use, e.g. "CubieB0T".
+        `auth`: Twitch OAuth Token, e.g. "oauth:pivogip8ybletucqdz4pkhag6itbax".
+        `callback`: The function or method that takes a `Message` object as parameter.
+        `capability`: List of strings with extra information to be requested from Twitch. See docs.
+        `live`: Boolean stating whether output messages should be posted in chat or only in the console.
+        """
+        assert type(host) == str
+        assert type(port) == int
+        assert (type(callback) == types.FunctionType or type(callback) == types.MethodType)
         threading.Thread.__init__(self)
+        # Thread parameters
         self.name = "TwitchWebsocket"
         self._stop_event = threading.Event()
+        self.daemon = True
 
-        # Variables
+        # Store passed variables
         self.host = host
         self.port = port
-        self.chan = chan
+        self.chan = chan if chan[0] == "#" else "#" + chan
         self.nick = nick
         self.auth = auth
         self.capability = capability
         self.callback = callback
         self.live = live
 
-        self.data = str()
-
-        # Lambda Functions.
-        self.send_join = lambda message, command="JOIN ": self._send(command, message)
-        self.send_pong = lambda message="", command="PONG ": self._send(command, message)
-        self.send_ping = lambda message="", command="PING ": self._send(command, message)
-        self.send_message = lambda message, command="PRIVMSG ": self._send("{}{} :".format(command, self.chan.lower()), message) if self.live else print(message)
-        self.send_whisper = lambda sender, message: self.send_message(f"/w {sender} {message}")
-        self.send_nick = lambda message, command="NICK ": self._send(command, message)
-        self.send_pass = lambda message, command="PASS ": self._send(command, message)
-        self.send_part = lambda message, command="PART ": self._send(command, message)
-        self.send_req = lambda message, command="CAP REQ :twitch.tv/": self._send(command, message)
-
     def start_nonblocking(self):
-        self._initialize_websocket()
+        """
+        Start the bot in the background, 
+        I.e. more code can be run immediately in the main thread. (hence, non-blocking)
+        """
         self.start()
+
+    def start_blocking(self):
+        """
+        Start the bot in the foreground, 
+        I.e. the bot needs to terminate before the main thread can continue. (hence, blocking)
+        """
+        self.start_nonblocking()
+        self.wait()
+        
+    def start_bot(self):
+        """
+        Deprecated version of `self.start_blocking()`.
+        """
+        self.start_blocking()
+
+    def wait(self):
+        """
+        Wait for the bot to terminate, while allowing `KeyboardInterrupt' to terminate the program.
+        """
+        try:
+            # Joining without a timeout will prevent the KeyboardInterrupt from triggering.
+            while self.is_alive():
+                self.join(1)
+        except (KeyboardInterrupt, SystemExit):
+            # Stop the while loop in run()
+            self.stop()
+            # Cancel the self.conn.recv() in run()
+            self.conn.shutdown(socket.SHUT_WR)
+            # Join this thread
+            self.join()
+            raise
+
+    def stop(self):
+        """
+        Set the event that indicates that the bot should stop.
+        The bot will stop receiving messages after the next message.
+        """
+        self._stop_event.set()
+    
+    def stopped(self) -> bool:
+        """
+        Returns the status of the event that indicates if the bot should stop.
+        True if `stop()` has been called. False otherwise.
+        """
+        return self._stop_event.is_set()
+
+    def run(self):
+        """
+        First connect to Twitch using `connect()`, and then collect messages from Twitch
+        indefinitely. `self.callback` is called with every message received.
+        Automatically reconnects on timeout.
+        """
+        self.connect()
+        # `data` will be continuously appended with received data
+        data = ""
+        while not self.stopped():
+            try:
+                # Receive data from Twitch Websocket.
+                data += self.conn.recv(8192).decode('UTF-8')
+            
+            except UnicodeDecodeError:
+                # In case of unexpected end of data.
+                logger.warning("Received data could not be decoded. Skipping this data.")
+                continue
+            
+            except OSError as e:
+                logger.error(f"[OSError: {e}] - Attempting to reconnect.")
+                self.connect()
+                continue
+            
+            data_split = data.split("\r\n")
+            data = data_split.pop()
+
+            # Iterate over seperately sent data.
+            for line in data_split:
+                m = Message(line)
+
+                # We will do some handling depending on the message ourself,
+                # so the developer doesn't have to.
+                if m.type == "PING":
+                    self.send_pong()
+
+                self.callback(m)
+
+    def _send(self, command: str, message: str):
+        """
+        Send data to Twitch, with the `command` message command, and `message` as the content.
+        In most cases, the public `send_...` methods should be used instead of this method,
+        e.g. `self.send_message(message)` or `self.send_whisper(message, user)`
+        """
+        sent = self.conn.send(bytes("{}{}\r\n".format(command, message), 'UTF-8'))
+        if sent == 0:
+            raise RuntimeError("Socket connection broken, sent is 0")
+    
+    def send_join(self, channel: str) -> None:
+        """
+        Send JOIN request over IRC to connect to `channel` their Twitch chat. 
+        
+        `channel` must be a string and nonempty. May be prepended with "#",
+        and can have any casing , e.g. "#Tom" is equivalent to "tom".
+        """
+        self._send("JOIN ", channel)
+
+    def send_part(self, channel: str) -> None:
+        """
+        Send PART request over IRC to disconnect from `channel` their Twitch chat. 
+        
+        `channel` must be a string and nonempty. May be prepended with "#",
+        and can have any casing , e.g. "#Tom" is equivalent to "tom".
+        """
+        self._send("PART ", channel)
+    
+    def send_pong(self) -> None:
+        """
+        Send PONG over IRC to Twitch.
+        """
+        self._send("PONG ", "")
+    
+    def send_ping(self) -> None:
+        """
+        Send PING over IRC to Twitch.
+        """
+        self._send("PING ", "")
+    
+    def send_message(self, message: str) -> None:
+        """
+        Send `message` in the connected Twitch Chat using the connected Twitch account,
+        but only if `self.live` is True.
+        If this boolean is False, simply print out `message`.
+        """
+        if self.live:
+            self._send("PRIVMSG {} :".format(self.chan.lower()), message)
+        else:
+            print(message)
+
+    def send_whisper(self, user: str, message: str):
+        """
+        Whisper `message` to `user` on Twitch using the connected Twitch account, 
+        but only if `self.live` is True.
+        If this boolean is False, simply print out `/w user message`.
+        """
+        self.send_message(f"/w {user} {message}")
+
+    def send_nick(self, nickname: str) -> None:
+        """
+        Send a NICK message to Twitch to identify the 
+        bot with the username/nickname `nickname`.
+        """
+        self._send("NICK ", nickname)
+
+    def send_pass(self, authentication):
+        """
+        Send a PASS message to Twitch to authenticate the 
+        bot with the authentication `authentication`.
+        """
+        self._send("PASS ", authentication)
+
+    def send_req(self, capability: str) -> None:
+        """
+        Send a CAP REQ request to add specific capability to access Twitch-specific
+        commands, data, etc. See https://dev.twitch.tv/docs/irc/guide#twitch-irc-capabilities
+        for more information.
+        """
+        self._send("CAP REQ ", ":twitch.tv/" + capability)
+
+    def connect(self):
+        """
+        Connect to Twitch, Log in using the nickname and authentication, join
+        the desired Twitch channel's chat, and optionally add capabilities for more
+        data from Twitch.
+        """
+        self._initialize_websocket()
         self.login(self.nick, self.auth)
         self.join_channel(self.chan)
         if self.capability is not None:
             self.add_capability(self.capability)
 
-    def start_bot(self):
-        try:
-            # Seting up the initial socket connection.
-            self.start_nonblocking()
-            
-            while not self.stopped(): # Loop to ensure that the try except is still active
-                time.sleep(1)
-        except (KeyboardInterrupt, SystemExit):
-            self.join()
-
-    def stop(self):
-        self._stop_event.set()
-    
-    def join(self):
-        # Stop the while loop in run()
-        self.stop()
-        # Cancel the self.conn.recv() in run()
-        self.conn.shutdown(socket.SHUT_WR)
-        # Join this thread
-        threading.Thread.join(self)
-    
-    def stopped(self):
-        return self._stop_event.is_set()
-
-    def run(self):
-        while not self.stopped():
-            try:
-                try:
-                    # Receive data from Twitch Websocket.
-                    packet = self.conn.recv(8192).decode('UTF-8')
-                except UnicodeDecodeError:
-                    # In case of unexpected end of data.
-                    continue
-
-                self.data += packet
-                data_split = self.data.split("\r\n")
-                self.data = data_split.pop()
-
-                # Iterate over seperately sent data.
-                for line in data_split:
-                    m = Message(line)
-
-                    # We will do some handling depending on the message ourself,
-                    # so the developer doesn't have to.
-                    if m.type == "PING":
-                        self.send_pong()
-
-                    self.callback(m)
-
-            except OSError as e:
-                logging.error(f"[OSError: at {time.strftime('%X')}]: {e} -> {line}")
-                self._initialize_websocket()
-                if len(self.nick) > 0:
-                    self.login(self.nick, self.auth)
-                if len(self.chan) > 1:
-                    self.join_channel(self.chan)
-                if self.capability is not None:
-                    self.add_capability(self.capability)
-
-    def _send(self, command, message):
-        # Send data back to Twitch.
-        sent = self.conn.send(bytes("{}{}\r\n".format(command, message), 'UTF-8'))
-        if sent == 0:
-            raise RuntimeError("Socket connection broken, sent is 0")
-
     def _initialize_websocket(self):
+        """
+        Set up socket connection with Twitch, with a timeout of 330 seconds.
+        After 330 seconds, the bot will timeout and automatically reconnect.
+        """
+        def get_reconnection_delay():
+            """
+            Yield exponential increase, starting with 0, 1, 2, 4, 8, 16, ...
+            and ending at 512, which is repeated infinitely.
+            """
+            yield 0
+            for val in (2**i for i in range(10)): # Yields 1, 2, ..., 256, 512
+                yield val
+            while True: # Yields 512, 512, 512, ...
+                yield val
+        
+        reconnection_delay_gen = get_reconnection_delay()
+
         while True:
             try:
-                logging.debug("Attempting to initialize websocket connection.")
+                logger.info("Attempting to initialize websocket connection.")
                 self.conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                
                 # We set the timeout to 330 seconds, as the PING from the Twitch server indicating
                 # That the connection is still live is sent roughly every 5 minutes it seems.
                 # the extra 30 seconds prevents us from checking the connection when it's not 
                 # needed.
-                
-                #TODO Error handle socket.gaierror: [Errno 11001] getaddrinfo failed and other errors
                 self.conn.settimeout(330)
                 
                 self.conn.connect( (self.host, self.port) )
-                logging.debug("Websocket connection initialized.")
+                logger.info("Websocket connection initialized.")
                 # Only return if successful
                 return
-            except socket.gaierror:
+            
+            except OSError:
                 # Sleep and retry if not successful
-                logging.debug("Failed to connect. Sleeping and retrying...")
-                time.sleep(5)
+                # reconnect_delay is 0, 1, 2, 4, 8, 16, ..., 512, 512, 512, ...
+                reconnect_delay = next(reconnection_delay_gen)
+                logger.error(f"Failed to connect. Sleeping for {reconnect_delay} seconds and retrying...")
+                time.sleep(reconnect_delay)
+            
+    def join_channel(self, channel: str) -> None:
+        """
+        Connect to the Twitch chat on the `channel` Twitch channel. 
+        Requires login using `self.login()`.
 
-    def join_channel(self, chan):
-        assert type(chan) == str and len(chan) > 0
+        `channel` must be a string and nonempty. May be prepended with "#",
+        and can have any casing , e.g. "#Tom" is equivalent to "tom".
+        """
+        assert type(channel) == str and channel
 
-        self.chan = chan if chan[0] == "#" else "#" + chan
-        self.send_join(self.chan.lower())
+        
+        self.send_join(channel.lower())
+
+    def leave_channel(self, channel: str) -> None:
+        """
+        Disconnect from the Twitch chat on the `channel` Twitch channel. 
+
+        `channel` must be a string and nonempty. May be prepended with "#",
+        and can have any casing , e.g. "#Tom" is equivalent to "tom".
+        """
+        self.send_part(channel.lower())
 
     def leave(self):
-        self.send_part(self.chan.lower())
+        """
+        Deprecated version of `self.leave_channel(channel)`.
+        """
+        self.leave_channel(self.chan)
 
-    def login(self, nick, auth):
-        assert type(nick) == type(auth) == str
+    def login(self, nickname: str, authentication: str):
+        """
+        Login using a nickname and corresponding authentication.
+        `nickname` and `authentication` must be a non-empty string.
+        """
+        assert type(nickname) == type(authentication) == str
+        assert nickname and authentication
 
-        self.nick = nick
-        self.auth = auth
-        self.send_pass(self.auth)
-        self.send_nick(self.nick.lower())
+        self.send_pass(authentication)
+        self.send_nick(nickname.lower())
 
-    def add_capability(self, capability):
+    def add_capability(self, capability: Union[str, List[str]]):
+        """
+        Add a capability on Twitch, i.e. request more information from Twitch.
+        `capability` must be a string or a list of strings. Currently, valid
+        capabilities are "membership", "tags" and "commands".
+        See https://dev.twitch.tv/docs/irc/guide#twitch-irc-capabilities for
+        more information.
+        """
         assert type(capability) in [list, str]
 
-        self.capability = capability
         if type(capability) == list:
             for cap in capability:
-                self.send_req(cap.lower())
+                self.add_capability(cap.lower())
         else:
             self.send_req(capability.lower())
